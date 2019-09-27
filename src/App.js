c51a14b14c0703d2c7a868ce31291d5be240a1b6
Santiago Desktop <santiagoclv@gmail.com>
2019-09-27 00:37:49 -0300

    Load Animations

--- src/App.js
+++ src/App.js
@@ -8,23 +8,29 @@ import './App.css';
 const { Header, Content } = Layout;
 
 function App() {
+
+  const refreshAnimation = (payload) => {
+    const player = document.querySelector('lottie-player');
+    player.load(payload);
+  };
+
+
   return (
     <Layout className="App">
       <Header>header</Header>
       <Layout>
         <Content>
           <Row gutter={8}  >
-            <Col span={18} >
+            <Col span={16} >
               <lottie-player
                 autoplay
                 controls
                 loop
                 mode="normal"
-                style={{width: "100%"}}
               >
               </lottie-player>
             </Col>
-            <Col span={6} className="AnimarionForm" ><AnimarionForm /></Col>
+            <Col span={8} className="AnimarionForm" ><AnimarionForm onLoadAnimation={refreshAnimation} /></Col>
           </Row>
         </Content>
       </Layout>
